f78bc96bd4c4c6441eda571630038c299fa49774
Alja Čekada <alja.cekada@gmail.com>
2023-04-06 19:32:14 +0200

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Experience from './pages/Experience'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <div className="App">
-<Router>
-  <Routes>
-    <Route path='/' element={<Home />}></Route>
-    <Route path='/projects' element={<Projects />}></Route>
-    <Route path='/experience' element={<Experience />}></Route>
-  </Routes>
-</Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/experience" element={<Experience />}></Route>
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "../styles/Navbar.css"
 
 function Navbar() {
   return (
-    <div></div>
+    <div className='navbar'>
+        <div className="toggleButton">
+            <button></button>
+        </div>
+        <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/experience">Experience</Link>
+        </div>
+    </div>
   )
 }
 
