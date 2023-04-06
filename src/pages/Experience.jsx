ff2d6bdaed341f6938260f499073ca20a1102504
Alja Čekada <alja.cekada@gmail.com>
2023-04-06 22:06:21 +0200

    create and style experience page

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+
+import "../styles/Experience.css";
 
 function Experience() {
   return (
-    <div>Experience</div>
-  )
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="Aug. 2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SubscriptionsIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Started watching Youtube tutorials
+          </h3>
+          <p>My journey started by watching various YouTube videos and exploring the world of software development, where I began to learn about the different possibilities within it.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="Oct. 2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<WorkspacePremiumIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            FreeCodeCamp Responsive Web Design Certification
+          </h3>
+          <p>Developer Certification, representing approximately 300 hours of coursework</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="Nov. - Dec. 2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<WorkspacePremiumIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Python for Everybody - University of Michigan Course
+          </h3>
+          <p>Learned basics of programming with Python</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="Sep. - Dec. 2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            SmartNinja Web Development Bootcamp
+          </h3>
+          <p>A 12-week Full-Stack Web Development Bootcamp</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="Jan. 2023 - present"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            The Odin Project Curriculum
+          </h3>
+          <p>Open source online curriculum for learning web development</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
-export default Experience
+export default Experience;
