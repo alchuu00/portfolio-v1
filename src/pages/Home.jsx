3d7cad0bc12ced539ca68b35384ef625523850da
Alja Čekada <alja.cekada@gmail.com>
2023-05-04 23:11:40 +0200

    add hero video to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,33 +3,53 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 import "../styles/Home.css";
+import HeroVideo from "../assets/hero_video.mp4";
+import Html from "../assets/html.png"
+import Css from "../assets/css.png"
+import Js from "../assets/js.png"
+import Ts from "../assets/ts.png"
+import Python from "../assets/python.png"
+import Sass from "../assets/sass.png"
+import Tailwind from "../assets/tailwind.png"
+import Bootstrap from "../assets/bootstrap.png"
+import React1 from "../assets/react.png"
+import Babel from "../assets/babel.png"
+import Git from "../assets/git.png"
+import Node from "../assets/nodejs.png"
+import Npm from "../assets/npm.png"
+import Webpack from "../assets/webpack.png"
+import Vscode from "../assets/vscode.png"
+import { Web } from "@mui/icons-material";
+
 
 function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2>Hi, my name is Alja</h2>
+        <div className="video-container">
+          <video src={HeroVideo} autoPlay loop muted></video>
+        </div>
+        <h2>Hi, my name is Alja👋</h2>
         <div className="prompt">
-          <p>A frontend developer with experience in healthcare.</p>
-          <a href="https://www.linkedin.com/in/alja-cekada/" target="_blank"><LinkedInIcon /></a>
-          <a href="https://github.com/alchuu00" target="_blank"><GitHubIcon /></a>
-          <a href="mailto:alja.cekada@gmail.com" target="_blank"><EmailIcon /></a>
+          <p>I design and code beautifully simple things, and I love what I do.👩‍💻</p>
+          <a href="https://www.linkedin.com/in/alja-cekada/" target="_blank">
+            <LinkedInIcon />
+          </a>
+          <a href="https://github.com/alchuu00" target="_blank">
+            <GitHubIcon />
+          </a>
+          <a href="mailto:alja.cekada@gmail.com" target="_blank">
+            <EmailIcon />
+          </a>
         </div>
       </div>
       <div className="skills">
         <h1>Skills</h1>
         <ol className="list">
           <li className="item">
-            <h2>Languages</h2>
-            <span>HTML, CSS, JavaScript, Python</span>
-          </li>
-          <li className="item">
-            <h2>Frameworks</h2>
-            <span>React, Flask, Bootstrap, TailwindCSS</span>
-          </li>
-          <li className="item">
-            <h2>Developer Tools</h2>
-            <span>Git, NPM, VS code, PyCharm, AWS (Heroku)</span>
+            <span><img src={Html}/><img src={Css}/><img src={Js}/><img src={Ts}/><img src={Node}/><img src={Sass}/><img src={Python}/></span>
+            <span><img src={Tailwind}/><img src={Bootstrap}/><img src={React1}/></span>
+            <span><img src={Git}/><img src={Npm}/><img src={Webpack}/><img src={Babel}/><img src={Vscode}/></span>
           </li>
         </ol>
       </div>
